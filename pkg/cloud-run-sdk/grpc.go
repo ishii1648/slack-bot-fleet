ed91fc89744a42db3d0c51737970b1c1cdcf7d7f
ishii1648 <ishiisho0509@gmail.com>
2021-05-29 11:44:58 +0900

    add grpc feature
    Signed-off-by: ishii1648 <ishiisho0509@gmail.com>

--- pkg/cloud-run-sdk/grpc.go
+++ pkg/cloud-run-sdk/grpc.go
@@ -2,11 +2,16 @@ package sdk
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"os"
+	"strings"
 
+	m "cloud.google.com/go/compute/metadata"
 	"github.com/rs/zerolog"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/metadata"
 )
 
@@ -32,7 +37,7 @@ func InjectLoggerInterceptor(rootLogger *zerolog.Logger) grpc.UnaryServerInterce
 			if traceID == "" {
 				return handler(ctx, req)
 			}
-			trace := fmt.Sprintf("projects/%s/traces/%s", projectID, traceID)
+			trace := fmt.Sprintf("projects/%s/traces/%s", ProjectID, traceID)
 
 			l.UpdateContext(func(c zerolog.Context) zerolog.Context {
 				return c.Str("logging.googleapis.com/trace", trace)
@@ -44,4 +49,52 @@ func InjectLoggerInterceptor(rootLogger *zerolog.Logger) grpc.UnaryServerInterce
 
 		return handler(ctx, req)
 	}
+}
+
+func InjectClientAuthInterceptor(idToken string) grpc.UnaryClientInterceptor {
+	return func(ctx context.Context, method string, req interface{}, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+		md := metadata.New(map[string]string{"authorization": fmt.Sprintf("Bearer %s", idToken)})
+		ctx = metadata.NewOutgoingContext(ctx, md)
+
+		return nil
+	}
+}
+
+func NewTLSConn(addr string) (*grpc.ClientConn, error) {
+	systemRoots, err := x509.SystemCertPool()
+	if err != nil {
+		return nil, err
+	}
+
+	cred := credentials.NewTLS(&tls.Config{
+		RootCAs: systemRoots,
+	})
+
+	idToken, err := getIDToken(addr)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := grpc.Dial(
+		addr,
+		grpc.WithAuthority(addr),
+		grpc.WithTransportCredentials(cred),
+		grpc.WithUnaryInterceptor(InjectClientAuthInterceptor(idToken)),
+	)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func getIDToken(addr string) (string, error) {
+	serviceURL := fmt.Sprintf("https://%s", strings.Split(addr, ":")[0])
+	tokenURL := fmt.Sprintf("/instance/service-accounts/default/identity?audience=%s", serviceURL)
+
+	idToken, err := m.Get(tokenURL)
+	if err != nil {
+		return "", err
+	}
+
+	return idToken, nil
 }
