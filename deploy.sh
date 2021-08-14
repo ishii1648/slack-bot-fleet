96124355c5e95d53b8acc29bc52e33fdf4671207
ishii1648 <ishiisho0509@gmail.com>
2021-08-14 22:13:04 +0900

    omit options on deploy shell
    Signed-off-by: ishii1648 <ishiisho0509@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,13 @@ SERVICE_NAME=$1
 if [ $SERVICE_NAME == "service-broker" ]; then
 	gcloud run deploy ${SERVICE_NAME} \
 		--platform=managed \
-		--region=asia-east1 \
-		--allow-unauthenticated \
+		--region=asia-northeast1 \
 		--image=asia.gcr.io/${GOOGLE_CLOUD_PROJECT}/${SERVICE_NAME}:latest \
-		--set-env-vars="SLACK_SIGNING_SECRET=${SLACK_SIGNING_SECRET}" \
-		--set-env-vars="SLACK_BOT_TOKEN=${SLACK_BOT_TOKEN}" \
 		--quiet
 elif [ $SERVICE_NAME == "example" ]; then
 	gcloud run deploy ${SERVICE_NAME} \
 		--platform=managed \
-		--region=asia-east1 \
+		--region=asia-northeast1 \
 		--image=asia.gcr.io/${GOOGLE_CLOUD_PROJECT}/${SERVICE_NAME}:latest \
-		--set-env-vars="SLACK_BOT_TOKEN=${SLACK_BOT_TOKEN}" \
 		--quiet
 fi
